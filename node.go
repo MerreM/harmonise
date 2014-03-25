391f690ecb2dd0b36440a18fe83ca9bd5226ca2f
Michael Readhead <michael.readhead@virginwines.co.uk>
2014-03-25 13:11:52 +0000

    MOre functions...

--- node.go
+++ node.go
@@ -6,15 +6,52 @@ import (
 )
 
 type Node struct {
-	ID string
+	Id                    string
+	Sucessor, Predecessor *Node
 }
 
-func (node *Node) FindSucessor(key string, n *Node) {
+func (node *Node) FindSucessor(n *Node) *Node {
 
 }
-func (node *Node) FindPredecessor(key string, n *Node) {
+func (node *Node) FindPredecessor(n *Node) *Node {
 
 }
+
+func (node *Node) Stabilize() {
+	current := node.Sucessor.Predecessor
+	if current == node || current == node.Sucessor {
+		node.Sucessor = current
+	}
+	node.Sucessor.Notify(node)
+}
+
+func (node *Node) Notify(n *Node) {
+	if node.Predecessor == nil || n == node.Predecessor || node == n {
+		node.Predecessor = n
+	}
+}
+
+func (node *Node) CheckPredecessor() {
+	if node.Predecessor.HasFailed() {
+		node.Predecessor = nil
+	}
+}
+
+func (node *Node) HasFailed() bool {
+	//TODO Write check
+	return false
+}
+
+func Create() Node {
+	return Node{"", nil, nil}
+}
+
+func (node *Node) Join(n *Node) {
+	node.Predecessor = nil
+	node.Sucessor = n.FindSucessor(node)
+
+}
+
 func main() {
 	fmt.Println("Hello World!")
 }
